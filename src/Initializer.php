128174d72bd0ffd1bc0f77fb349bb8e80ffa5077
Florian Moser <git@famoser.ch>
2026-09-07 10:50:55 +0200

    feat: Add data treatment

--- src/Initializer.php
+++ src/Initializer.php
@@ -2,14 +2,117 @@
 
 namespace Famoser\Mamapp;
 
+use PhpOffice\PhpSpreadsheet\IOFactory;
 use Psr\Http\Message\StreamInterface;
 
 class Initializer
 {
+    public const string MAMMALS_DIR = 'mammals';
+    public const string SOURCE_DIR = PathHelper::VAR_PERSISTENT_DIR . DIRECTORY_SEPARATOR . self::MAMMALS_DIR;
+    public const string CACHE_DIR = PathHelper::VAR_TRANSIENT_DIR . DIRECTORY_SEPARATOR . self::MAMMALS_DIR;
+    public const string TARGET_DIR = PathHelper::PUBLIC_DIR . DIRECTORY_SEPARATOR . self::MAMMALS_DIR;
+
     public static function init(StreamInterface $output): void
     {
-        // read out xlsx, transform to JSON
-        // find all images, resize, copy to /public/data/images
-        // copy mammals.json to /public/data
+        if (!self::getAnimalsXlsxFilepath($output, $path)) {
+            return;
+        }
+
+        $output->write("Found file at " . $path . "\n");
+
+        if (!self::xlsxToArray($output, $path, $animals)) {
+            return;
+        }
+
+        $output->write("Found " . count($animals) . " animals.\n");
+
+
+        if (!self::writeJsonToTargetDir($output, $animals, "mammals.json")) {
+            return;
+        }
+
+        $output->write("Created mammals.json.\n");
+    }
+
+    /**
+     * @phpstan-assert-if-true string $path
+     */
+    private static function getAnimalsXlsxFilepath(StreamInterface $output, ?string &$path): bool
+    {
+        $files = glob(self::SOURCE_DIR . DIRECTORY_SEPARATOR . '*.xlsx');
+
+        if (!$files || count($files) !== 1) {
+            $output->write("No unique XLSX file found in: self::SOURCE_DIR\n");
+            return false;
+        }
+
+        $path = $files[0];
+        return true;
+    }
+
+    /**
+     * @param string[] $data
+     *
+     * @phpstan-assert-if-true string[] $data
+     */
+    private static function xlsxToArray(StreamInterface $output, string $filePath, ?array &$data = null): bool
+    {
+        $spreadsheet = IOFactory::load($filePath);
+        $worksheet = $spreadsheet->getActiveSheet();
+
+        $data = [];
+        $headers = [];
+        $isFirstRow = true;
+        $expectedHeader = ['Species_ID_prov','Deutscher Name','Wissenschaftlicher Name','mdd_id','iucn_status','Familie','Beschreibung','Ähnliche Arten','Lebensraum','Wie finden','Naturschutz'];
+        $cleanHeader = ['id', 'name', 'latinName', 'mddId', 'iucnStatus', 'family', 'similarSpecies', 'habitat', 'observe', 'convervation'];
+
+        foreach ($worksheet->getRowIterator() as $row) {
+            $rowData = [];
+
+            foreach ($row->getCellIterator() as $cell) {
+                $rowData[] = $cell->getValue();
+            }
+
+            if ($isFirstRow) {
+                if ($rowData !== $expectedHeader) {
+                    $output->write("Fail: Columns not in expected format / order. Expected: " . join(", ", $expectedHeader) . "Actual: " . join(", ", $rowData) . "\n");
+                    return false;
+                }
+
+                $isFirstRow = false;
+                continue;
+            }
+
+            if (empty($rowData[0])) {
+                continue;
+            }
+
+            $data[] = array_combine($cleanHeader, $rowData);
+        }
+
+        return true;
+    }
+
+    /**
+     * @param string[] $data
+     */
+    private static function writeJsonToTargetDir(StreamInterface $output, array $data, string $filename): bool
+    {
+        if (!is_dir(self::TARGET_DIR)) {
+            if (!mkdir(self::TARGET_DIR, 0755, true)) {
+                $output->write("Error: Failed to create target directory: " . (self::TARGET_DIR) . "\n");
+                return false;
+            }
+        }
+
+        $jsonFile = self::TARGET_DIR . DIRECTORY_SEPARATOR . $filename;
+        $json = json_encode($data, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
+
+        if (file_put_contents($jsonFile, $json) === false) {
+            $output->write("Error: Failed to write JSON file: $jsonFile\n");
+            return false;
+        }
+
+        return true;
     }
 }
--- src/PathHelper.php
+++ src/PathHelper.php
@@ -4,8 +4,9 @@ namespace Famoser\Mamapp;
 
 class PathHelper
 {
-    public const string VAR_DIR = __DIR__ . '/../var';
+    public const string ROOT_DIR = __DIR__ . '/..';
+    public const string VAR_DIR = self::ROOT_DIR . '/var';
     public const string VAR_TRANSIENT_DIR = self::VAR_DIR . '/transient';
     public const string VAR_PERSISTENT_DIR = self::VAR_DIR . '/persistent';
-    public const string CONFIG_DIR = self::VAR_DIR . '/config';
+    public const string PUBLIC_DIR = self::ROOT_DIR . '/public';
 }
