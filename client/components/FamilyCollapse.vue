8a067cb9a62c9fe8ed162343bcbcd36131cdf66a
Florian Moser <git@famoser.ch>
2026-09-08 15:09:16 +0200

    fix: Fix collapsing animation

--- client/components/FamilyCollapse.vue
+++ client/components/FamilyCollapse.vue
@@ -38,15 +38,22 @@ const toggleCollapse = () => {
 
 <template>
   <div>
-    <div v-if="header" class="d-flex justify-content-between text-primary-emphasis p-2 bg-primary-subtle rounded-top-2" role="button" @click="toggleCollapse" :aria-expanded="true" aria-controls="collapse-body">
+    <div
+      v-if="header"
+      class="d-flex justify-content-between text-primary-emphasis p-2 bg-primary-subtle rounded-top-2"
+      role="button"
+      @click="toggleCollapse"
+      :aria-expanded="true"
+      aria-controls="collapse-body"
+    >
       <p class="mb-1 fw-bold">{{ family }}</p>
       <span class="d-flex align-items-center gap-2 text-muted small">
         <span>{{ animals.length }} species</span>
         <i class="icon icon-chevron-left" :class="{ 'animate-turn-ccw': !collapseState }"></i>
       </span>
     </div>
-    <div ref="collapseElement" id="collapse-body" class="collapse show bg-primary-subtler p-1">
-      <div class="d-flex flex-column gap-1">
+    <div ref="collapseElement" id="collapse-body" class="collapse show">
+      <div class="d-flex flex-column gap-1 bg-primary-subtler p-1">
         <animal-preview v-for="animal in animals" :key="animal.id" :animal="animal" :is-favorite="favorites.includes(animal.id)" @toggle-favorite="emit('toggle-favorite', animal)" />
       </div>
     </div>
