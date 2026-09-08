fbc78d3d60c0d49fd2498390be61b6934bea4b7a
Florian Moser <git@famoser.ch>
2026-09-08 16:18:48 +0200

    feat: View single mammal

--- client/pages/MammalPage.vue
+++ client/pages/MammalPage.vue
@@ -2,20 +2,36 @@
 import { useRoute } from 'vue-router'
 import { useTranslator } from '@/locales/translator'
 import { useMammalsState } from '@/services/state'
+import { useDatabase } from '@/services/data'
+import { computed } from 'vue'
+import ImageCarousel from '@/components/ImageCarousel.vue'
 
 const route = useRoute()
-const mammalId = route.params.id
+const speciesId = route.params.id
 
 const { t } = useTranslator()
 
 const { favorites } = useMammalsState()
+
+const { species } = useDatabase()
+const animal = computed(() => {
+  return species.find((s) => s.id === speciesId)
+})
 </script>
 
 <template>
-  <div class="mb-4">
-    <h3 class="mb-2">{{ t('pages.title') }}</h3>
-    {{ mammalId }}
+  <div v-if="animal">
+    <image-carousel :images="animal.images" />
+    <h1 class="text-primary-emphasis mt-2 mb-1">{{ animal.name }}</h1>
+    <p class="text-muted">
+      <i>{{ animal.latinName }}</i>
+    </p>
 
-    {{ favorites.length }}
+    <p>{{ animal.description }}</p>
+
+    <p>{{ animal.similarSpecies }}</p>
+    <p>{{ animal.habitat }}</p>
+    <p>{{ animal.observe }}</p>
+    <p>{{ animal.conservation }}</p>
   </div>
 </template>
