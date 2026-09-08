e0882478bf4935dfeb2c17008f08b197f164ab5b
Florian Moser <git@famoser.ch>
2026-09-08 17:13:44 +0200

    feat: Design animal view

---
+++ client/components/AnimalText.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+defineProps<{
+  icon: string
+  label: string
+  text: string
+}>()
+</script>
+
+<template>
+  <div class="border border-1 border-primary-subtle p-2 rounded-1 position-relative">
+    <div class="d-flex align-items-center gap-2 mb-2 shift-up bg-white px-2">
+      <i class="icon" :class="icon" />
+      <span class="text-muted">{{ label }}</span>
+    </div>
+    {{ text }}
+  </div>
+</template>
+
+<style scoped>
+.shift-up {
+  position: absolute;
+  top: -1em;
+}
+</style>
